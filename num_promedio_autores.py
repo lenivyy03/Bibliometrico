2e91873c252bd0cdd18dc2f0effe75db08f1de62
Fernanda Herrnandez <a224220205@unison.mx>
2026-04-19 04:44:15 -0700

    Cambie el return a -1 en caso de errores
    Change return values for error handling in num_promedio_autores function.

--- num_promedio_autores.py
+++ num_promedio_autores.py
@@ -1,14 +1,14 @@
 # Función que regresa las estadísticas del número promedio de autores entre todos los articulos
 def num_promedio_autores(df):
     # Verificamos que se encuentra información la columna de Autores
-    if df is None: return 0
-    if 'Authors' not in df.columns: return 0
+    if df is None: return -1
+    if 'Authors' not in df.columns: return -1
 
     # Limpiamos las celdas vacías de la columna de Autores
     documentos = df['Authors'].dropna()
 
     # Verificamos que después de evitar celdas vacías aun queden celdas validas con autores
-    if(documentos.empty): return 0 
+    if(documentos.empty): return -1 
 
     num_autores = 0 # Variable que almacenará la cantidad de autores de toda la recopilación
     
